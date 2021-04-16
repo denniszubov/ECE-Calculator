1dbdb2fd55eb7bf46140668de44a7f862895ae3c
dennis.zubov <denniszubov365@gmail.com>
2021-04-16 14:12:21 -0400

    Updated the way the input is translated

--- helper_methods.py
+++ helper_methods.py
@@ -1,22 +1,23 @@
 # This method removes all the unwanted spaces from a string.
-# It strips the spaces around the string and removes and double spaces
+# It strips the spaces around the string and removes and double spaces.
 def remove_spaces(line: str):
     return " ".join(line.split())
 
 
-# This method calculates the number of different characters in a string
-def number_diff_chars(line: str):
+# This method returns a list with all the unique characters in the string.
+def characters(line: str):
     line = line.lower()
-    chars = {}
+    chars = set()
     for c in line:
         if c in "abcdefghijklmnopqrstuvwxyz":
-            chars[c] = 1
-
-    return len(chars)
+            chars.add(c)
+    return_chars = list(chars)
+    return_chars.sort()
+    return return_chars
 
 
 # This method translates the boolean expression into an expressino that python
-# can evaluate and returns the number of inputs
+# can evaluate, returns the number of inputs, and the input characters.
 def translate_input(bool_expression: str):
     bool_expression = remove_spaces(bool_expression)
     bool_expression = bool_expression.upper()
@@ -25,7 +26,7 @@ def translate_input(bool_expression: str):
     bool_expression = bool_expression.replace("NOT", "~")
     bool_expression = bool_expression.replace("XOR", "^")
 
-    num_inputs = number_diff_chars(bool_expression)
-    input_info = {"expression": bool_expression, "inputs": num_inputs}
+    input_chars = characters(bool_expression)
+    input_info = {"expression": bool_expression, "num_inputs": len(input_chars), "input_chars": input_chars}
 
     return input_info
